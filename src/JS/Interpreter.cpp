ca796f1af9893d6db899feee8526cfb583e81648
Osama Khaliq <osamakhaliq97@gmail.com>
2020-09-10 19:23:39 +0100

    JS: Use new execute() function in ASTNodes.
    This replaces the evaluate function that was part of the old visitor
    pattern implementation.

--- src/JS/Interpreter.cpp
+++ src/JS/Interpreter.cpp
@@ -4,9 +4,9 @@ Interpreter::Interpreter()
 
 }
 
-void Interpreter::intereprt(std::shared_ptr<Expression<Value> > expr)
+void Interpreter::intereprt(std::shared_ptr<Expression> expr)
 {
-    std::shared_ptr<Value> value = evaluate(expr);
+    std::shared_ptr<Value> value = expr->execute();
     printf("%s\n", value->toString().c_str());
 }
 
