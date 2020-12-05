429b46f46ffa72032262695475466d5e557f4674
Osama Khaliq <osamakhaliq97@gmail.com>
2020-12-05 18:45:59 +0000

    Give variable types different colours in terminal
    When the value of a variable is returned and printed to the terminal,
    they are now printed in different colours as shown below.
    
    Number = Yellow
    String = Green
    Undefined = black

--- src/JS/Interpreter.cpp
+++ src/JS/Interpreter.cpp
@@ -7,7 +7,23 @@ Interpreter::Interpreter()
 void Interpreter::intereprt(std::shared_ptr<Expression> expr)
 {
     std::shared_ptr<Value> value = expr->execute();
-    printf("%s\n", value->toString().c_str());
+    if (value->type == Value::ValueType::NUMBER || value->type == Value::ValueType::BOOL )
+    {
+        printf("\033[33m%s\033[0m\n", value->toString().c_str());
+    }
+
+    if (value->type == Value::ValueType::STRING)
+    {
+        printf("\033[32m'%s'\033[0m\n", value->toString().c_str());
+    }
+
+    if (value->type == Value::ValueType::UNDEFINED)
+    {
+        printf("\033[30m%s\033[0m\n", value->toString().c_str());
+    }
+
+
+    //printf("%s\n", value->toString().c_str());
 }
 
 Interpreter::~Interpreter()
