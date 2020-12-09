50213e0c49d63a38dd010b685db141feb012d1bd
Osama Khaliq <osamakhaliq97@gmail.com>
2020-12-09 19:55:41 +0000

    Add CSS token class for use by CSS tokenizer
    These are the data structures used for individual tokens that the new
    CSS tokenizer produces.

--- src/CSS/CSSToken.h
+++ src/CSS/CSSToken.h
@@ -1,4 +1,130 @@
 #ifndef CSSTOKEN_H
 #define CSSTOKEN_H
+#include <string>
+enum class CSSTokenType
+{
+    Whitespace,
+    String,
+    BadString,
+    Hash,
+    Delim,
+    LeftParenthesis,
+    RightParenthesis,
+    Comma,
+    CDC,
+    Colon,
+    Semicolon,
+    LeftSquareBracket,
+    RightSquareBracket,
+    LeftCurlyBracket,
+    RightCurlyBracket,
+    Number,
+    Percentage,
+    Dimension,
+    IdentLike,
+    EndOfFile
+};
+
+enum class HashType
+{
+    ID,
+    Unrestricted
+};
+
+enum class NumberType
+{
+    Integer,
+    Number
+};
+
+class Number
+{
+public:
+    std::string repr;
+    NumberType type;
+};
+
+static std::string CSSTokenTypes[] =
+{
+    "Whitespace",
+    "String",
+    "BadString",
+    "Hash",
+    "Delim",
+    "LeftParenthesis",
+    "RightParenthesis",
+    "Comma",
+    "CDC",
+    "Colon",
+    "Semicolon",
+    "LeftSquareBracket",
+    "RightSquareBracket",
+    "LeftCurlyBracket",
+    "RightCurlyBracket",
+    "Number",
+    "Percentage",
+    "Dimension",
+    "IdentLike",
+    "EndOfFile"
+};
+
+
+class CSSToken
+{
+public:
+    CSSToken(CSSTokenType type) : type(type)
+    {
+        if (type == CSSTokenType::Hash)
+            this->m_hashType = HashType::Unrestricted;
+    }
+
+    CSSTokenType type;
+    std::string unit;
+
+    void setValue(std::string value)
+    {
+        this->m_value = value;
+    }
+
+    void appendValue(std::string value)
+    {
+        this->m_value += value;
+    }
+
+    void appendValue(char value)
+    {
+        this->m_value += value;
+    }
+
+    std::string value()
+    {
+        return m_value;
+    }
+
+    void setHashTokenType(HashType value)
+    {
+        this->m_hashType = value;
+    }
+
+    HashType hashType()
+    {
+        return m_hashType;
+    }
+
+    void setNumberType(NumberType value)
+    {
+        this->m_numberType = value;
+    }
+
+    NumberType numberType()
+    {
+        return m_numberType;
+    }
+
+private:
+    std::string m_value = "";
+    HashType m_hashType;
+    NumberType m_numberType;
+};
 
 #endif // CSSTOKEN_H
