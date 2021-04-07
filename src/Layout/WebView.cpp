f8531ef8e9ae01fb44d16b03be32af2386283aff
Osama Khaliq <osamakhaliq97@gmail.com>
2021-04-07 17:51:42 +0100

    Remove setting of width and height of the WebView widget
    This is now done in the HTMLView once it's figured out the height of all
    the layout boxes. We can't do that here as we don't have the layout
    boxes.

--- src/Layout/WebView.cpp
+++ src/Layout/WebView.cpp
@@ -4,8 +4,6 @@ WebView::WebView(Core* core, const std::shared_ptr<Node> renderElements) : m_cor
 {
     printf("Init WebView\n");
     htmlView = std::make_unique<HTMLView>(core, renderElements);
-    htmlView->set_size_request(htmlView->maxWidth, htmlView->maxHeight);
-
 
     this->add(*htmlView);
     this->show_all();
